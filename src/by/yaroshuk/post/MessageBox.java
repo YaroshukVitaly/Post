7c997407418c11f8c444709743eaa76442b1ad68
Vitalya <yaroshukvi@gmail.com>
2018-08-20 14:11:05 +0300

    ArrayList вместо массива

--- src/by/yaroshuk/post/MessageBox.java
+++ src/by/yaroshuk/post/MessageBox.java
@@ -1,30 +1,32 @@
 package by.yaroshuk.post;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MessageBox {
 
-    private Message[] messages;
-    private int current;
+    private List<Message> messages;
     private static long nextIndex = 0;
 
-    public MessageBox(int count) {
-        messages = new Message[count];
-        current = 0;
+    public MessageBox(){
+        this (100) ;
     }
+    public MessageBox(int count) {
+        messages = new ArrayList<Message>(count);
+        }
 
     public long add(Message.MassageCategory category, String sender, String address, String receiver){
         Message message = new Message(category, sender, address, receiver);
         message.setId(nextIndex++);
-        messages [current] = message;
-        current++;
-        return message.getId();
+        messages.add(message);
+                return message.getId();
     }
 
     @Override
     public String toString() {
         return "MessageBox{" +
-                "messages=" + Arrays.toString(messages) +
+                "messages=" + messages +
                 '}';
     }
 }
